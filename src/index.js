8b303cb903b0773aae2cf539b9a5cf1942f21702
DenisRudchenko <xpyctemo@gmail.com>
2023-02-16 21:35:13 +0300

    feat:result

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let result = [];
+  let halfMatrix = [];halfMatrix
+  let secondHalfMatrix = {};
+
+  for(let i = 0; i < bracketsConfig.length; i++) {
+    halfMatrix.push(bracketsConfig[i][0]);
+    secondHalfMatrix [bracketsConfig[i][1]] = bracketsConfig[i][0];
+  }
+  for(let i = 0; i < str.length; i++) {
+    if(secondHalfMatrix[str[i]] === result[result.length - 1] && result.length !== 0) result.pop(); 
+    else if(halfMatrix.includes(str[i])) result.push(str[i]); 
+    else if(result.length === 0) return false;
+  }
+  return result.length === 0;
 }
